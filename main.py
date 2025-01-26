da8dc38f1b3b692def5587fc4e743a9bb47512d2
mpuscian <puscian.m@gmail.com>
2025-01-26 01:08:44 +0100

    open file via input path added

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Init")
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Pass path to file")
+    parser.add_argument("--path", help="Path to the file")
+    args = parser.parse_args()
+    print(f"Path given: {args.path}")
+    f = open(args.path, "r")
+    print(f.read())
+
+
+if __name__ == "__main__":
+    main()
