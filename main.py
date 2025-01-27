35e3163d97b1b9b92a505178ec3e99f7ac5c3ead
mpuscian <puscian.m@gmail.com>
2025-01-27 22:08:22 +0100

    preprocessing enabled

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import subprocess
 from pathlib import Path
 
@@ -6,6 +7,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("file_path", type=Path)
 p = parser.parse_args()
 f = open(p.file_path, "r")
-print(f.name)
-cmd = "gcc " + f.name
+print(f.name)  # prints file path
+preprocessed_file = os.path.basename(f.name)
+preprocessed_file = preprocessed_file[:-1] + "i"
+print(preprocessed_file)
+cmd = "gcc -E -P " + f.name + " -o " + preprocessed_file  # command ran by shell
 subprocess.Popen(cmd, shell=True)
