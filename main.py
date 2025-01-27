92ebe7e33e877e88f5455eb6ed4dea96841b0f82
mpuscian <puscian.m@gmail.com>
2025-01-27 21:27:52 +0100

    added run file by gcc

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import argparse
+import subprocess
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file_path", type=Path)
 p = parser.parse_args()
 f = open(p.file_path, "r")
-print(f.read())
+print(f.name)
+cmd = "gcc " + f.name
+subprocess.Popen(cmd, shell=True)
