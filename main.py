384914f5b5ec01d6751242d8f390b17c796bca7f
mpuscian <puscian.m@gmail.com>
2025-02-03 23:33:36 +0100

    added flags

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file_path", type=Path)
+parser.add_argument("--lex", action="store_true", default=False)
+parser.add_argument("--parse", action="store_true", default=False)
+parser.add_argument("--codegen", action="store_true", default=False)
+
 p = parser.parse_args()
+if p.lex:
+    print("Place for lexer init")
+if p.parse:
+    print("Place for parse init")
+if p.codegen:
+    print("Place for codegen init")
 f = open(p.file_path, "r")
 print(f.name)  # prints file path
 preprocessed_file = os.path.basename(f.name)
