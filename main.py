ae50645915a4950a333a1103d982e68bd0e4f7cc
mpuscian <puscian.m@gmail.com>
2025-02-15 23:18:43 +0100

    regex added

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
     )  # command ran by shell
     subprocess.Popen(cmd, shell=True)
     if p.lex:
-        tokenizing(preprocessed_file)
+        tokenization(preprocessed_file)
     if p.parse:
         print("Place for parse init")
     if p.codegen:
@@ -33,13 +33,40 @@ def main():
     return 0
 
 
-def tokenizing(filename):
+def tokenization(filename):
     file = open(filename, "r")
     single_string = file.read()  # returns file content as signle string
     # r'' - string raw literal
     pattern_identifier = r"[A-zA-Z_]\w*\b"
-    x = re.findall(pattern_identifier, single_string)
-    print(x)
+    pattern_constant = r"[0-9]+\b"
+    pattern_int_keyword = r"int\b"
+    pattern_void_keyword = r"void\b"
+    pattern_return_keyword = r"return\b"
+    pattern_op_par = r"\("
+    pattern_clse_par = r"\)"
+    pattern_op_brace = r"{"
+    pattern_clse_brace = r"}"
+    patter_semicolon = r";"
+    semicolons_found = re.findall(patter_semicolon, single_string)
+    constant_found = re.findall(pattern_constant, single_string)
+    identifiers_found = re.findall(pattern_identifier, single_string)
+    ints_found = re.findall(pattern_int_keyword, single_string)
+    voids_found = re.findall(pattern_void_keyword, single_string)
+    returns_found = re.findall(pattern_return_keyword, single_string)
+    op_pars_found = re.findall(pattern_op_par, single_string)
+    clse_pars_found = re.findall(pattern_clse_par, single_string)
+    op_braces_found = re.findall(pattern_op_brace, single_string)
+    clse_braces_found = re.findall(pattern_clse_brace, single_string)
+    print(constant_found)
+    print(identifiers_found)
+    print(ints_found)
+    print(returns_found)
+    print(voids_found)
+    print(op_pars_found)
+    print(clse_pars_found)
+    print(op_braces_found)
+    print(clse_braces_found)
+    print(semicolons_found)
     return 0
 
 
